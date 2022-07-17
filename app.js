30b82dda8bbde2c2ac34bca490aded9df9b4f7db
Kevin Leister <kevinleister07@gmail.com>
2022-07-17 14:02:15 -0500

    create header, footer, about templates. create getDate module, require it , and use it to pass in the current day

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
 // npm init, npm install express body-parser ejs
 // nodemon app.js to start server
 
-// require express and body-parser
+// require express, body-parser, date module
 const express = require("express");
 const bodyParser = require("body-parser");
-var items = ["Buy Food", "Cook Food", "Eat Food"];
-// use express and body-parser
+const date = require(__dirname + "/date.js");
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+// use express, body-parser, and create static public folder for css
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 // set up views directory and template for ejs to utilize
 app.set('view engine', 'ejs');
 // get and render data for the current day, also renders new list items
 app.get("/", function (req, res) {
-    var today = new Date();
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-    };
-    var day = today.toLocaleDateString("en-US", options);
-    res.render("list", {kindOfDay: day, newListItems: items});
+let day = date();
+    res.render("list", {listTitle: day, newListItems: items});
 });
 // receive new item entry and push to item array
 app.post("/", function(req, res){
-   var item = req.body.newItem;
-   items.push(item);
- res.redirect("/");
+   let item = req.body.newItem;
+   if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+   } else {
+       items.push(item);
+     res.redirect("/");
+   }
+});
+// create a work list
+app.get("/work", function(req, res){
+res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+app.post("/work", function(req, res){
+let item = req.body.newItem;
+workItems.push(item);
+res.redirect("/work");
+});
+// about page route
+app.get("/about", function(req, res){
+res.render("about");
 });
 // express local server
 app.listen(3000, function () {
