ddc90fe2a13cb8cda4b26d7c2f89698e9303d41d
Kevin Leister <kevinleister07@gmail.com>
2022-07-18 12:16:50 -0500

    refactor code to simplify

--- app.js
+++ app.js
@@ -5,8 +5,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 // use express, body-parser, and create static public folder for css
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,12 +15,12 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 // get and render data for the current day, also renders new list items
 app.get("/", function (req, res) {
-let day = date();
+const day = date.getDate();
     res.render("list", {listTitle: day, newListItems: items});
 });
 // receive new item entry and push to item array
 app.post("/", function(req, res){
-   let item = req.body.newItem;
+   const item = req.body.newItem;
    if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
@@ -34,7 +34,7 @@ app.get("/work", function(req, res){
 res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 app.post("/work", function(req, res){
-let item = req.body.newItem;
+const item = req.body.newItem;
 workItems.push(item);
 res.redirect("/work");
 });
